741ac2c9b8af7c7cb95894c26e0045500c8e276f
demidraksin <greatraksin@icloud.com>
2022-06-21 18:55:15 +0300

    Finishing project

--- routes.py
+++ routes.py
@@ -1,7 +1,12 @@
+import sqlite3
 from app import app
 from flask import render_template, redirect, url_for, request, flash
 
-messages = []
+
+def db_connect():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  # выгружаю строки из базы данных
+    return conn
 
 
 @app.route('/')  # пишу функцию для главной страницы
@@ -18,15 +23,26 @@ def about():
         phone = request.form['phone']
 
         if not name:  # если имя не вписали
-            flash('Поле "имя" обязательно!')
+            flash('Поле "имя" обязательно!', 'warning')
         elif not email:
-            flash('Поле email обязательно')
+            flash('Поле email обязательно', 'warning')
         else:
-            messages.append({'username': name, 'email': email, 'phone': phone})
-            return redirect(url_for('main_page'))
+            try:
+                connection = db_connect()
+                connection.execute('INSERT INTO users (name, email, phone) VALUES (?, ?, ?)',
+                                   (name, email, phone))
+                connection.commit()  # применить изменения в БД
+                connection.close()
+                flash('Форма успешно отправлена!', 'success')
+                return redirect(url_for('about'))
+            except sqlite3.IntegrityError:
+                flash('Пользователь с таким email уже существует!', 'danger')
+                return redirect(url_for('about'))
     return render_template('about.html')
 
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html', user_messages=messages)
+    connection = db_connect()
+    users = connection.execute('SELECT * FROM users')
+    return render_template('admin.html', users=users)
